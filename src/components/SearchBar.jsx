33ba12a26431f07e4382e472e3865d4a84660c2a
fazildev12 <fazilfazillisaf17@gmail.com>
2024-07-17 07:03:13 +0530

    fetch data from json api and filtered

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaSearch } from "react-icons/fa";
 import './SearchBar.css'
 
 
 export default function SearchBar() {
+  const [input, setInput] = useState("")
+
+  const fetchData = (value) => {
+    const url = 'https://jsonplaceholder.typicode.com/users';
+    
+    fetch(url).then(res => res.json())
+      .then(json => {
+        const results = json.filter((user) => {
+          return (
+            value &&
+            user &&
+            user.name &&
+            user.name.toLowerCase().includes(value)
+        );
+      });
+      console.log(results)
+  })
+}
+
+  const handleChange = (value) => {
+    setInput(value)
+    fetchData(value)
+  }
+
+
   return (
     <div className='input-wrapper'>
-      <FaSearch id='search-icon'/>
-      <input placeholder='type to search..' />
+      <FaSearch id='search-icon' />
+      <input
+        placeholder='type to search..'
+        value={input}
+        onChange={(e) => handleChange(e.target.value)}
+      />
     </div>
   )
 }
